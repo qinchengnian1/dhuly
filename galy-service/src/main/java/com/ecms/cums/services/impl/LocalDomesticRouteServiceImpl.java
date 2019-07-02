e50f38c5e202ebf3bfca54c3a163b16fdae50dd1
qin12345 <qinchengnian1@163.com>
2019-07-02 10:16:08 +0800

    123

--- galy-service/src/main/java/com/ecms/cums/services/impl/LocalDomesticRouteServiceImpl.java
+++ galy-service/src/main/java/com/ecms/cums/services/impl/LocalDomesticRouteServiceImpl.java
@@ -189,7 +189,9 @@ public class LocalDomesticRouteServiceImpl  implements LocalDomesticRouteService
 		c.andAgentTypeEqualTo(jsonbody.getString("agentType"));
 		c.andCityIdsLike("%"+jsonbody.getString("cityId")+"%");
 		c.andStatusEqualTo("1");
-		c.andSystemFlagEqualTo("1");	
+		if (StringUtils.isNotBlank(jsonbody.getString("systemFlag "))){
+			c.andSystemFlagEqualTo(jsonbody.getString("systemFlag "));
+		}
 		List<Product> ce=productMapper.selectByExample(criteria);
 		return ce;
 	}
